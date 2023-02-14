e18202cd8d7451fb1822530c08c94fd514ec8e47
GaboSO21 <gso2090@gmail.com>
2023-02-13 20:45:14 -0800

    Semana 5 - Service & CRUD

--- techshop/src/main/java/com/techshop/techshop/Controller/ClienteController.java
+++ techshop/src/main/java/com/techshop/techshop/Controller/ClienteController.java
@@ -3,7 +3,9 @@ package com.techshop.techshop.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import com.techshop.techshop.Model.Cliente;
 import com.techshop.techshop.Service.*;
 
 @Controller
@@ -17,8 +19,37 @@ public class ClienteController {
 
     @GetMapping("/")
     public String inicio(Model model) {
-        model.addAttribute("clientes", clienteService.listarClientes());
         return "index";
+    }
+
+    @GetMapping("/cliente/listar")
+    public String listar(Model model) {
+        model.addAttribute("clientes", clienteService.listarClientes());
+        return "listar";
+    }
+
+    @GetMapping("/cliente/agregar")
+    public String agregarCliente(Cliente cliente) {
+        return "agregar";
+    }
+
+    @PostMapping("/cliente/guardar")
+    public String guardarCliente(Cliente cliente) {
+        clienteService.saveCliente(cliente);
+        return "redirect:/cliente/listar";
+    }
+
+    @GetMapping("/cliente/eliminar/{id_cliente}")
+    public String eliminarCliente(Cliente cliente) {
+        clienteService.deleteCliente(cliente);
+        return "redirect:/cliente/listar";
+    }
+
+    @GetMapping("/cliente/editar/{id_cliente}")
+    public String editarCliente(Cliente cliente, Model model) {
+        cliente = clienteService.getCLiente(cliente);
+        model.addAttribute("cliente", cliente);
+        return "agregar";
     }
 
 }
--- techshop/src/main/java/com/techshop/techshop/Service/ClienteService.java
+++ techshop/src/main/java/com/techshop/techshop/Service/ClienteService.java
@@ -3,6 +3,7 @@ package com.techshop.techshop.Service;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.techshop.techshop.Model.Cliente;
 import com.techshop.techshop.Repository.*;
@@ -16,8 +17,25 @@ public class ClienteService {
         this.clienteRepository = clienteRepository;
     }
 
+    // Makes query READ ONLY, transaction much faster
+    @Transactional(readOnly = true)
     public List<Cliente> listarClientes() {
         return (List<Cliente>) clienteRepository.findAll();
+    }
+
+    @Transactional
+    public Cliente getCLiente(Cliente cliente) {
+        return clienteRepository.findById(cliente.getId_cliente()).orElse(null);
+    }
+
+    @Transactional
+    public void saveCliente (Cliente cliente) {
+       clienteRepository.save(cliente); 
+    } 
+
+    @Transactional
+    public void deleteCliente (Cliente cliente) {
+        clienteRepository.deleteById(cliente.getId_cliente());
     }
 
 }
